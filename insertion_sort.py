369b550007d4bdf963993d82c5d7ca81736f47a9
Rosenkilde <50791042+rosenkilde95@users.noreply.github.com>
2020-02-24 12:23:57 +0100

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,17 @@
+sort_me = [9, 2, 1, 4, 7, 6, 5, 3, 8]
+
+def insertionsort(list):
+    for index in range(1, len(list)):
+        current = list[index]
+        position = index
+
+        while position > 0 and list[position-1] > current:
+            print("Swapped {} for {}".format(list[position],
+                                            list[position-1]))
+            list[position] = list[position-1]
+            print(list)
+            position -= 1
+        list[position] = current
+    return list
+
+print(insertionsort(sort_me))
